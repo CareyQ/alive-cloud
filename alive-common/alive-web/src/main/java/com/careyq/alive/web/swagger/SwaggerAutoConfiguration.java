c20c780ef071789f92c9a9acfe2c69f5f41a34d8
CareyQ <careyq03@gmail.com>
2023-11-15 17:37:14 +0800

    feat: nacos 集成

---
+++ alive-common/alive-web/src/main/java/com/careyq/alive/web/swagger/SwaggerAutoConfiguration.java
@@ -0,0 +1,42 @@
+package com.careyq.alive.web.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Swagger 自动配置
+ *
+ * @author CareyQ
+ * @since 2023-11-15
+ */
+@AutoConfiguration
+@ConditionalOnClass({OpenAPI.class})
+@EnableConfigurationProperties(SwaggerProperties.class)
+@ConditionalOnProperty(prefix = "springdoc.api-docs", name = "enabled", havingValue = "true", matchIfMissing = true)
+public class SwaggerAutoConfiguration {
+
+    @Bean
+    public OpenAPI openAPI(SwaggerProperties properties) {
+        return new OpenAPI()
+                .info(buildInfo(properties));
+    }
+
+    /**
+     * API 摘要
+     */
+    private Info buildInfo(SwaggerProperties properties) {
+        return new Info()
+                .title(properties.getTitle())
+                .description(properties.getDescription())
+                .version(properties.getVersion())
+                .contact(new Contact().name(properties.getAuthor()).url(properties.getUrl()).email(properties.getEmail()))
+                .license(new License().name(properties.getLicense()).url(properties.getLicenseUrl()));
+    }
+}
---
+++ alive-common/alive-web/src/main/java/com/careyq/alive/web/undertow/UndertowAutoConfiguration.java
@@ -0,0 +1,27 @@
+package com.careyq.alive.web.undertow;
+
+import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+/**
+ * Undertow 容器配置
+ *
+ * @author CareyQ
+ * @since 2023-11-15
+ */
+@AutoConfiguration
+public class UndertowAutoConfiguration implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+
+    @Override
+    public void customize(UndertowServletWebServerFactory factory) {
+        // 缓冲池配置
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> {
+            WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
+            webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 1024));
+            deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
+        });
+    }
+}
