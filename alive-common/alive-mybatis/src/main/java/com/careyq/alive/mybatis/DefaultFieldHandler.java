c8c2f96d22333ac72e70749a009a160f5a82f6d5
CareyQ <careyq03@gmail.com>
2023-11-17 11:56:18 +0800

    feat: MyBatis 配置

---
+++ alive-common/alive-mybatis/src/main/java/com/careyq/alive/mybatis/DefaultFieldHandler.java
@@ -0,0 +1,38 @@
+package com.careyq.alive.mybatis;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.careyq.alive.core.domain.BaseEntity;
+import org.apache.ibatis.reflection.MetaObject;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * 参数填充
+ *
+ * @author CareyQ
+ * @since 2023-11-17
+ */
+public class DefaultFieldHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        if (Objects.nonNull(metaObject) && metaObject.getOriginalObject() instanceof BaseEntity entity) {
+            LocalDateTime now = LocalDateTime.now();
+            if (Objects.isNull(entity.getCreateTime())) {
+                entity.setCreateTime(now);
+            }
+            if (Objects.isNull(entity.getUpdateTime())) {
+                entity.setUpdateTime(now);
+            }
+        }
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        Object modifyTime = getFieldValByName("updateTime", metaObject);
+        if (Objects.isNull(modifyTime)) {
+            setFieldValByName("updateTime", LocalDateTime.now(), metaObject);
+        }
+    }
+}
---
+++ alive-common/alive-mybatis/src/main/java/com/careyq/alive/mybatis/MyBatisAutoConfiguration.java
@@ -0,0 +1,32 @@
+package com.careyq.alive.mybatis;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * MyBatis 自动配置
+ *
+ * @author CareyQ
+ * @since 2023-11-17
+ */
+@AutoConfiguration
+@MapperScan(value = "${alive.info.base-package}", annotationClass = Mapper.class)
+public class MyBatisAutoConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+
+    @Bean
+    public MetaObjectHandler defaultMetaObjectHandler() {
+        return new DefaultFieldHandler();
+    }
+}
