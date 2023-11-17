b573b0a02456a0d2b3155f37f09d7ac647e6d009
CareyQ <careyq03@gmail.com>
2023-11-17 11:26:19 +0800

    feat: 全局异常、jackson 配置、xss 配置

--- alive-common/alive-web/src/main/java/com/careyq/alive/web/jackson/LocalDateTimeDeserializer.java
+++ alive-common/alive-web/src/main/java/com/careyq/alive/web/jackson/LocalDateTimeDeserializer.java
@@ -1,7 +1,6 @@
 package com.careyq.alive.web.jackson;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
