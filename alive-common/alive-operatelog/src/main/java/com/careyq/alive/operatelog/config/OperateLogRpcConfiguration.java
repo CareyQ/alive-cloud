fc602fb5a076d1d66c7d96a149249a6b909e89fd
CareyQ <careyq03@gmail.com>
2024-03-01 15:36:43 +0800

    feat: mall 转 spring cloud

---
+++ alive-common/alive-operatelog/src/main/java/com/careyq/alive/operatelog/config/OperateLogRpcConfiguration.java
@@ -0,0 +1,15 @@
+package com.careyq.alive.operatelog.config;
+
+import com.careyq.alive.module.infra.api.LogApi;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 操作日志 rpc 配置
+ *
+ * @author CareyQ
+ */
+@AutoConfiguration
+@EnableFeignClients(clients = {LogApi.class})
+public class OperateLogRpcConfiguration {
+}
---
+++ alive-mall/alive-product-service/src/main/java/com/careyq/alive/module/product/MallApplication.java
@@ -0,0 +1,17 @@
+package com.careyq.alive.module.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * mall 模块
+ *
+ * @author CareyQ
+ */
+@SpringBootApplication
+public class MallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+}
