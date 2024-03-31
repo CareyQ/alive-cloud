172612a45b233fef70c8b495547fdea6cf4231f1
CareyQ <careyq03@gmail.com>
2024-03-31 17:08:28 +0800

    feat: ES 配置修改

--- alive-search/alive-search-service/src/main/java/com/careyq/alive/search/config/EsAutoConfiguration.java
+++ alive-search/alive-search-service/src/main/java/com/careyq/alive/search/config/EsAutoConfiguration.java
@@ -6,6 +6,9 @@ import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -23,11 +26,22 @@ public class EsAutoConfiguration {
     @Value("${es.host}")
     private String host;
 
+    @Value("${es.username}")
+    private String username;
+
+    @Value("${es.password}")
+    private String password;
+
     private final ObjectMapper objectMapper;
 
     @Bean
     public ElasticsearchClient elasticsearchClient() {
-        RestClient client = RestClient.builder(HttpHost.create(host)).build();
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClient client = RestClient.builder(HttpHost.create(host))
+                .setHttpClientConfigCallback(e -> e.setDefaultCredentialsProvider(credentialsProvider))
+                .build();
 
         RestClientTransport transport = new RestClientTransport(client, new JacksonJsonpMapper(objectMapper));
         return new ElasticsearchClient(transport);
